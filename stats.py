4be9f36d1a8f8b1c38e98d9ed0047ce324e4678b
s131800 <phippsee3@gmail.com>
2025-11-23 18:09:30 -0500

    added comment to stats

--- stats.py
+++ stats.py
@@ -21,6 +21,8 @@ def sortList(dictionary):
     '''for item in dictSort:
         if str(item).isalpha() == False:
             remove dictSort[item]'''
+    
+    #take the values out of the dictionary and sort them in a separate list. Then we need to create a tuple where we take the sorted value and search through the original dictionary for the value to pull the letter. Then put the letter and value in the output from the tupled list. Big Brain
     print(dictSort)
 
 characterCount("books/frankenstein.txt")
